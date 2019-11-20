d97dcd1c4490904e6dc248fc3c164d135af975bc
Schaller Simon <simon.scahller@lernende.bfo-vs.ch>
2019-11-20 10:51:01 +0100

    Add name Input field

--- idex.php
+++ idex.php
@@ -1,3 +1,12 @@
+<?php
+$name = "";
+if(isset($_GET['name'])){
+$name = $_GET['name'];
+}
+?>
+
+
+
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
@@ -5,15 +14,26 @@
 <body>
 <?php
 echo "<h1>GTA V</h1>";
-echo "<p1>viel Spass</p1>";
+echo "<p>viel Spass</p>";
+echo "<p>".$name."</p>";
 ?>
-<br>
+
+<form>
+<input type="text" name="name">
+<input type="submit">
+</form>
+
+<p style="color:red;">WASTED</p>
 <audio controls>
   <source src="GTAV.mp3" type="audio/mpeg">
 </audio>
 <br>
+<p1>Error</p1>
+<br>
 <audio controls>
 <source src="WINDOWSXPError.mp3" type="audio/mpeg">
 </audio>
+
+	
 </body>
 </html>
